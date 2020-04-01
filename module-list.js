9ed6c7466b0fa7c396904c766b992e65a525e7ff
Zhiming Zhou <zzm.vmiis@gmail.com>
2020-04-01 18:25:41 +1100

    update

---
+++ module-list.js
@@ -0,0 +1,11 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var modules={
+        "reports":  	{url:"$H/m/reports.html",router:1},
+    }
+    for(p in modules){
+        $vm.module_list[p]=modules[p]; 
+        $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
+    }
+    //-------------------------------------------------------------------------------------
+})();
