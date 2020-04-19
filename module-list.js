38faa4fdb8858319d74e6a13d32250a889537e0f
Zhiming Zhou <zzm.vmiis@gmail.com>
2020-04-20 08:28:54 +1000

    update

--- module-list.js
+++ module-list.js
@@ -2,6 +2,7 @@
     //-------------------------------------------------------------------------------------
     var modules={
         "reports":  	{url:"$H/m/reports.html",router:1},
+        "about":  	    {url:"$H/0/about.html",router:1},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
